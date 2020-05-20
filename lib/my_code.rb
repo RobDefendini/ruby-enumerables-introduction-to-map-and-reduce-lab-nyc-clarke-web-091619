c6b54a4259a968820835b9b94b14e74dfc164272
Robert Defendini <robdefendini@gmail.com>
2020-05-20 23:29:29 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -55,7 +55,7 @@ end
 def reduce_to_all_true(array)
     counter = 0 
     while counter < array.size do 
-      return false if |e| e&& 
+      return false if array  
     counter += 1
     end
     return true
